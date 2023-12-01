a5f1ebd025f9651d160f767e9766ccd9a666fd5a
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2023-12-01 19:44:20 +0100

    Añadido un estado que es una lista de productos de Room

--- app/src/main/java/com/marcossan/despensa/states/ProductsState.kt
+++ app/src/main/java/com/marcossan/despensa/states/ProductsState.kt
@@ -2,6 +2,10 @@ package com.marcossan.despensa.states
 
 import com.marcossan.despensa.models.Product
 
+/**
+ * Son los elementos que forman parte de los estados y a su vez, cuándo estos elementos reciban
+ * modificaciones por parte del usuario, se redibujará la IU.
+ */
 data class ProductsState(
     val products: List<Product> = emptyList()
 )
