f0944e16d2876f3d00a36c10922148c4a88b144c
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2023-12-01 19:25:57 +0100

    Añadida la entidad products de Room

---
+++ app/src/main/java/com/marcossan/despensa/models/Products.kt
@@ -0,0 +1,27 @@
+package com.marcossan.despensa.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entidad products de la base de datos local
+ */
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo("code")
+    val code: String,
+    @ColumnInfo("name")
+    var name: String,
+    @ColumnInfo("imageUrl")
+    val imageUrl: String,
+    @ColumnInfo("expirationDate")
+    val expirationDate: String = "No disponible",
+    @ColumnInfo("dateAdded")
+    val dateAdded: String = "",
+    @ColumnInfo("quantity")
+    val quantity: String = "1"
+)
+
